0fc3f4216ae069860f8a984c803bcb9dd448039a
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-01-15 23:15:14 +0100

    fixes

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ window.addEventListener('mousemove', (e) => {
     cursorEl.style.top = e.pageY + 'px'
     cursorEl.setAttribute('data-fromTop', cursorEl.offsetTop - scrollY)
 })
+let active = 'active';
+let wrong = undefined;
 window.addEventListener('click', function () {
 
     if (cursorEl.classList.contains('click')) {
         cursorEl.classList.remove('click')
-        void cursorEl.offsetWidth;
+        cursorEl.offsetWidth;
         cursorEl.classList.add('click')
     } else {
         cursorEl.classList.add('click')
